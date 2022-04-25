54a26c7f06a8bd1aaae284691942053658dbb718
Robert Gessler <ragessler@Roberts-MacBook-Pro.local>
2022-04-25 11:36:48 -0500

    completed problem 2

--- app.js
+++ app.js
@@ -145,6 +145,22 @@ console.log('Vegetarian Foods: ', vegetarianFood)
 
 //2. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
 //Filter
+function problemTwo(){
+
+    let results = dishes.filter(function(element){
+        if (element.cuisine === 'Italian' && element.servings >= 5){
+            return true;
+        }
+        else{
+            return false;
+        }})
+        return results
+}
+
+let bigItalianFood = problemTwo();
+console.log('Italian food with 5+ servings:', bigItalianFood)
+// Complete
+``
 
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
